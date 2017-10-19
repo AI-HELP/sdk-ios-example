48d270b83d6de3732f6a4411db8627b1222f5a00
guojialin <guojialin@cs30.net>
2017-10-19 12:10:02 +0800

    update sdk

---
+++ aihelpsdk/ElvaChatServiceSDK.framework/Headers/ElvaChatServiceSDK.h
@@ -0,0 +1,19 @@
+//
+//  ElvaChatServiceSDK.h
+//  ElvaChatServiceSDK
+//
+//  Created by wwj on 16/7/21.
+//  Copyright © 2016年 wwj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ElvaChatServiceSDK.
+FOUNDATION_EXPORT double ElvaChatServiceSDKVersionNumber;
+
+//! Project version string for ElvaChatServiceSDK.
+FOUNDATION_EXPORT const unsigned char ElvaChatServiceSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ElvaChatServiceSDK/PublicHeader.h>
+
+
