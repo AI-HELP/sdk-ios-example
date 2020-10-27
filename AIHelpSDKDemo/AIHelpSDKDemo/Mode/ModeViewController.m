3f1965dd682bcd152aa778b8878c2fa0f73c5bc5
Payne.R <huyang_work@163.com>
2020-10-27 16:58:53 +0800

    update demo

--- AIHelpSDKDemo/AIHelpSDKDemo/Mode/ModeViewController.m
+++ AIHelpSDKDemo/AIHelpSDKDemo/Mode/ModeViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     self.title = @"mode";
 }
+
 - (IBAction)humanClick:(UIButton *)sender {
     AIHelpConversationConfigBuilder *conversationBuilder = [[AIHelpConversationConfigBuilder alloc] init];
     conversationBuilder.conversationIntent = AIHelpConversationIntentHumanSupport;
