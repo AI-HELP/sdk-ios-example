0dc34f82a4d75abf504ac09dfa50e0195f4ec997
Payne-R <renpeng@aihelp.net>
2020-10-29 15:26:49 +0800

    update demo

--- AIHelpSDKDemo/Pods/Target Support Files/Pods-AIHelpSDKDemo/Pods-AIHelpSDKDemo-resources.sh
+++ AIHelpSDKDemo/Pods/Target Support Files/Pods-AIHelpSDKDemo/Pods-AIHelpSDKDemo-resources.sh
@@ -97,10 +97,10 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${PODS_ROOT}/AIHelpSDK/AIHelp-iOS-SDK/AIHelpSDK/AIHelpSupportSDK.bundle"
+  install_resource "${PODS_ROOT}/AIHelpSDK/AIHelpSDK/AIHelpSupportSDK.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${PODS_ROOT}/AIHelpSDK/AIHelp-iOS-SDK/AIHelpSDK/AIHelpSupportSDK.bundle"
+  install_resource "${PODS_ROOT}/AIHelpSDK/AIHelpSDK/AIHelpSupportSDK.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
