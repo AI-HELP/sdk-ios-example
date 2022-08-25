96c5a4b0fa0494f62434fea67aeb0a1c59597b13
Joseph_L <joseph_l@126.com>
2022-08-25 15:13:05 +0800

    update sdk v4.1.3

--- AIHelpSDK/AIHelpSupportSDK.framework/Headers/AIHelpSDKConfig.h
+++ AIHelpSDK/AIHelpSupportSDK.framework/Headers/AIHelpSDKConfig.h
@@ -56,44 +56,14 @@ typedef NS_ENUM(int, AIHelpFAQSupportEntrance) {
 - (AIHelpUserConfig *)build;
 @end
 
+#pragma mark - AIHelpApiConfig
 
-#pragma mark - ECServiceConversationConfig
-
-@interface AIHelpConversationConfig : NSObject
+@interface AIHelpApiConfig : NSObject
 - (id) init NS_UNAVAILABLE;
 @end
 
-@interface AIHelpConversationConfigBuilder : NSObject
-@property (nonatomic, assign)AIHelpConversationIntent conversationIntent;    // show elva bot page or show conversation page
-@property (nonatomic, assign)BOOL alwaysShowHumanSupportButtonInBotPage;         // default is NO.if you set ture,user can always see the contact us button
-@property (nonatomic, copy)NSString *welcomeMessage;           // default is http://aihelp.net/dashboard setting. you can show different welcome msg by different users with this param, It has a higher priority.
-@property (nonatomic, copy)NSString *storyNode;                   // set specific story node for specific scene. With this api call, you can show different stories in different scenes. the story node's User Say content you configured in aihelp admin dashboard.(http://aihelp.net/dashboard)
-- (AIHelpConversationConfig *)build;
-@end
-
-
-#pragma mark - ECServiceFAQConfig
-
-@interface AIHelpFAQConfig : NSObject
-- (id) init NS_UNAVAILABLE;
-@end
-
-@interface AIHelpFAQConfigBuilder : NSObject
-@property (nonatomic, assign)AIHelpFAQShowConversationMoment showConversationMoment;      // see enum -> ElvaFAQShowConversationMoment
-@property (nonatomic, strong)AIHelpConversationConfig *conversationConfig;           // see config -> ECServiceConversationConfig
-- (AIHelpFAQConfig *)build;
-@end
-
-
-#pragma mark - ECServiceOperationConfig
-
-@interface AIHelpOperationConfig : NSObject
-- (id) init NS_UNAVAILABLE;
-@end
-
-@interface AIHelpOperationConfigBuilder : NSObject
-@property (nonatomic, assign)int selectIndex;                                  // default is elva tab.  If you set a negative index, or index larger than the total tab counts, the selected tab will still be elva.
-@property (nonatomic, copy)NSString *conversationTitle;                        // default is "HELP", you can change the operation conversation bot title
-@property (nonatomic, strong)AIHelpConversationConfig *conversationConfig;            // see config -> ECServiceConversationConfig
-- (AIHelpOperationConfig *)build;
+@interface AIHelpApiConfigBuilder : NSObject
+@property (nonatomic, copy)NSString *entranceId;
+@property (nonatomic, copy)NSString *welcomeMessage;
+- (AIHelpApiConfig *)build;
 @end
