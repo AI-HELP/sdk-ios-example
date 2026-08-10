835540bdcf198dd71a85a99fe39f007f3d643b40
Joseph_L <joseph_l@126.com>
2026-08-10 15:03:40 +0800

    publish SDK 6.1.1

---
+++ AIHelpSDK/AIHelpSupportSDK.framework/Headers/AIHelpSDKConfig.h
@@ -0,0 +1,102 @@
+//
+//  AIHelpSDKConfig
+//
+//  Created by AIHelp.
+//  Copyright © 2020 aihelp.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(int,AIHelpTokenPlatform) {                    /* PushTokenPlatform enum*/
+    AIHelpTokenPlatformAPNS               = 1,                // Apple APNS
+    AIHelpTokenPlatformFirebase           = 2,                // firebase-FCM
+    AIHelpTokenPlatformJpush              = 3,                // Jpush
+    AIHelpTokenPlatformGeTui              = 4,                // GeTui
+    AIHelpTokenPlatformHUAWEI             = 6,                // HUAWEI
+    AIHelpTokenPlatformOneSignal          = 7,                // OneSignal
+};
+
+typedef NS_ENUM(int,AIHelpFAQShowConversationMoment) {             /* ConversationMoment enum, show ContactUs moment */
+    AIHelpFAQShowConversationMomentNever            = 1,           // Never show
+    AIHelpFAQShowConversationMomentAlways           = 2,           // Always show
+    AIHelpFAQShowConversationMomentAfterMarkingUnhelpful = 3,      // Show after unhelpful with Faq detail
+    AIHelpFAQShowConversationMomentOnlyInAnswerPage = 4,           // only in answer page show
+};
+
+typedef NS_ENUM(int,AIHelpPublishCountryOrRegion) {
+    AIHelpCN = 1,
+    AIHelpIN
+};
+
+typedef NS_ENUM(int, AIHelpFAQSupportEntrance) {
+    AIHelpFAQSupportEntranceHomePage = 1,
+    AIHelpFAQSupportEntranceQuestionList = 2,
+    AIHelpFAQSupportEntranceAnswerPage = 3,
+    AIHelpFAQSupportEntranceAfterMarkingUnhelpful = 4,
+    AIHelpFAQSupportEntranceFAQNotFound = 5,
+};
+
+typedef NS_ENUM(int, AIHelpLoginStatus) {
+    AIHelpLoginSuccess = 1,
+    AIHelpInvalidUID = -1,
+    AIHelpAuthError = -2,
+};
+
+typedef NS_ENUM(int, AIHelpEventType) {
+    AIHelpEventInitialization,                      // Event for SDK initialization
+    AIHelpEventUserLogin,                           // Event for user login
+    AIHelpEventEnterpriseAuth,                      // Event for enterprise authentication
+    AIHelpEventSessionOpen,                         // Event for opening a session (window)
+    AIHelpEventSessionClose,                        // Event for closing a session (window)
+    AIHelpEventMessageArrival,                      // Event for message arrival
+    AIHelpEventLogUpload,                           // Event for log upload
+    AIHelpEventUrlClick,                            // Event for URL click
+    AIHelpEventUnreadTaskCount,                     // Event for Task unread count
+    AIHelpEventConversationStart,                   // Event for conversation start
+};
+
+typedef void (*AISupportAsyncEventListener)(const char *jsonEventData, void (*acknowledge)(const char *jsonAckData));
+
+#pragma mark - ECServiceUserConfig
+
+@interface AIHelpUserConfig : NSObject
+- (id) init NS_UNAVAILABLE;
+@end
+
+@interface AIHelpUserConfigBuilder : NSObject
+@property (nonatomic, copy)NSString       *userName;      // default is "anonymous"
+@property (nonatomic, copy)NSString       *avatarUrl;     // default is nil
+@property (nonatomic, copy)NSString       *serverId;      // default is nil
+@property (nonatomic, strong)NSArray        *userTags;      // If you assign this field with existing tags from aihelp admin dashboard, the tickets created by current user will take these tags by default.
+@property (nonatomic, strong)NSDictionary   *customData;    // Set custom meta data you want to see in the aihelp admin dashboard.
+@property (nonatomic, assign)BOOL           isSyncCrmInfo;  // If you set this to true, when you update current user's information, the sdk will sync user's information to you crm database.
+- (AIHelpUserConfig *)build;
+@end
+
+#pragma mark - AIHelpApiConfig
+
+@interface AIHelpApiConfig : NSObject
+- (id) init NS_UNAVAILABLE;
+@end
+
+@interface AIHelpApiConfigBuilder : NSObject
+@property (nonatomic, copy)NSString *entranceId;
+@property (nonatomic, copy)NSString *welcomeMessage;
+- (AIHelpApiConfig *)build;
+@end
+
+#pragma mark - AIHelpLoginConfig
+
+@interface AIHelpLoginConfig : NSObject
+- (id) init NS_UNAVAILABLE;
+@end
+
+@interface AIHelpLoginConfigBuilder : NSObject
+
+@property (nonatomic, copy) NSString *userId;
+
+@property (nonatomic, strong) AIHelpUserConfig *userConfig;
+
+- (AIHelpLoginConfig *)build;
+
+@end
---
+++ AIHelpSDK/AIHelpSupportSDK.framework/Headers/AIHelpSupportSDK.h
@@ -0,0 +1,161 @@
+//
+//  AIHelpSupportSDK
+//
+//  Created by AIHelp.
+//  Copyright © 2020 aihelp.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AIHelpSupportSDK/AIHelpSDKConfig.h>
+#import <UIKit/UIKit.h>
+
+@interface AIHelpSupportSDK : NSObject
+
+/**
+ * Initialize AIHelp sdk
+ *
+ * When initializing AIHelp you must pass these three tokens. You initialize AIHelp by adding the following lines in the implementation file for your app delegate, ideally at the top of application:didFinishLaunchingWithOptions
+ * @param domainName This is your domain name without any http:// or forward slashes
+ * @param appId  This is the unique ID assigned to your app
+ */
++ (void)initializeWithDomainName:(NSString *)domainName appId:(NSString *)appId;
+
+/**
+ * Initialize AIHelp sdk
+ *
+ * When initializing AIHelp you must pass these three tokens. You initialize AIHelp by adding the following lines in the implementation file for your app delegate, ideally at the top of application:didFinishLaunchingWithOptions
+ * @param domainName This is your domain name without any http:// or forward slashes
+ * @param appId  This is the unique ID assigned to your app
+ * @param language  This is your expected init language
+ */
++ (void)initializeWithDomainName:(NSString *)domainName appId:(NSString *)appId language:(NSString *)language;
+
+/**
+ * Logs in a user using their user ID.
+ * * @param userId the unique identifier of the user to log in
+ */
++ (void)loginWithUserId:(NSString *)userId;
+
+/**
+ * Logs in a user using a LoginConfig object.
+ * <p>
+ * Checkout {@link LoginConfig} for more information.
+ *
+ * @param config the configuration object containing login details.
+ * @see LoginConfig
+ */
++ (void)login:(AIHelpLoginConfig *)config;
+
+/**
+ * Show the AIHelp conversation screen.
+ *
+ * If you want to custom your welcome message, please check next method for more information.
+ */
++ (BOOL)showWithEntranceId:(NSString *)entranceId;
+
+/**
+ Show the AIHelp conversation screen.
+ */
++ (BOOL)showWithApiConfig:(AIHelpApiConfig *)apiConfig;
+
++ (void)showSingleFAQ:(NSString *)faqId showConversationMoment:(AIHelpFAQShowConversationMoment)showConversationMoment;
+
+/**
+ * Update a user's profile via UserConfig.
+ *
+ * Please check ECServiceUserConfigfor more detail information.
+ * @param config configs which contains all information about a user.
+ */
++ (void)updateUserInfo:(AIHelpUserConfig *)config;
+
+/**
+ * Clear the values set to a user, reset the userId to deviceId, userName to 'anonymous'.
+ */
++ (void)resetUserInfo;
+
+/**
+ * Change the SDK language. By default, the device's prefered language is used.
+ *
+ * The call will fail in the following cases :
+ * 1. If a AIHelp session is already active at the time of invocation
+ * 2. Language code is incorrect
+ * 3. Corresponding localization file is not found
+ * @param sdkLanguage the string representing the language code. For example, use 'fr' for French.
+ */
++ (void)updateSDKLanguage:(NSString*)sdkLanguage;
+
+/**
+ * Set log path for uploading.
+ *
+ * In order to serve your customers well, you can upload customer-related-logs when tickets are created or
+ * specific forms are submitted.
+ * @param path the absolute path of log, which will be uploaded when needed
+ */
++ (void)setUploadLogPath:(NSString*)path;
+
+/**
+ * set the pushToken and platform to enable push notifications.
+ *
+ * To enable push notifications in the Helpshift iOS SDK, set the Push Notifications’ deviceToken using this method inside your application:didRegisterForRemoteNotificationsWithDeviceToken application delegate.
+ * NOTE: You must get the specific push sdk in your App BEFORE this invocation.
+ *
+ * @param pushToken    the pushToken received from the push notification servers.
+ * @param pushPlatform the specific push platform, please check ElvaTokenPlatform  for more information.
+ */
++ (void)setPushToken:(NSString*)pushToken pushPlatform:(AIHelpTokenPlatform)pushPlatform;
+
+/**
+ * Get current AIHelp SDK version
+ * @return AIHelp SDK version
+ */
++ (NSString*)getSDKVersion;
+
+/**
+ * Whether AIHelp session is visible to users
+ * @return whether sdk is active
+ */
++ (BOOL)isAIHelpShowing;
+
+/**
+ * Whether to print logs.
+ *
+ * It only works in Debug mode
+ * @param enable YES/NO
+ */
++ (void)enableLogging:(BOOL)enable;
+
+/**
+ * Unregisters an event listener for a specific event type.
+ *
+ * @param eventType The type of event to stop listening for.
+ */
++ (void)registerAsyncListener:(AISupportAsyncEventListener)asyncEventListener
+                    eventType:(AIHelpEventType)eventType;
+
++ (void)unregisterAsyncListenerWithEvent:(AIHelpEventType)eventType;
+
+/**
+ * Fetch unread message count proactively
+ */
++ (void)fetchUnreadMessageCount;
+
++ (void)fetchUnreadTaskCount;
+
+/**
+ * AIHelp provide additional support for some country or regions.
+ *
+ * @param countryOrRegion ISO country code, please check https://www.iso.org/obp/ui/#search to learn more.
+ */
++ (void)additionalSupportFor:(AIHelpPublishCountryOrRegion)countryOrRegion;
+
++ (void)showUrl:(NSString *)url;
+
++ (void)setKeyWindow:(UIWindow *)keyWin;
+
++ (void)close;
+
++ (void)logout;
+
++ (void)uninstall;
+
+@end
